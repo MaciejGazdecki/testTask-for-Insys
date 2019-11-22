8d3c5274b6893394cc58ee1c69830cb42ddf73c4
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-11-22 22:25:37 +0100

    profile section done and styled

--- src/js/App/App.js
+++ src/js/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route,Link} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route,Link} from 'react-router-dom';
 import styles from './app.module.scss';
 import Header from "../Components/Header/header";
 import Navigation from "../Components/Navigation/navigation";
